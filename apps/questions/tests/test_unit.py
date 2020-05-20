34efe0c7412bed5ca566e78f9fc8a9ecd3b315e1
Vinicius Issa <vinicius.almeidaissa@gmail.com>
2020-05-20 19:29:09 -0300

    [test][questions]Created units test for questions
    Created pytest.ini file and created tests for Question and Choice models

---
+++ apps/questions/tests/test_unit.py
@@ -0,0 +1,58 @@
+import pytest
+from apps.questions.models import Choice, Question
+
+
+@pytest.fixture
+def new_question(django_db_blocker):
+    question_title = "How are you?"
+    with django_db_blocker.unblock():
+        question = Question(question=question_title)
+        question.save()
+    return question
+
+
+@pytest.fixture(scope="module")
+def new_question_with_incorrect_choices(django_db_blocker):
+    titles = ["Choice1", "Choice2", "Choice3", "Choice4"]
+    question_title = "How are you?"
+    with django_db_blocker.unblock():
+        question = Question(question=question_title)
+        question.save()
+        for title in titles:
+            choice = Choice(
+                choice=title,
+                is_correct=False,
+                question=question
+            )
+            choice.save()
+    return question
+
+
+@pytest.mark.django_db
+def test_create_question(new_question):
+    assert new_question.id == 1
+
+
+@pytest.mark.django_db
+def test_create_choices(new_question):
+    choice_title = "Fine, Thanks"
+    choice = Choice(
+        choice=choice_title,
+        is_correct=False,
+        question=new_question
+    )
+    choice.save()
+    assert choice.id == 1
+
+
+@pytest.mark.django_db
+def test_question_exist_correct_choices(new_question_with_incorrect_choices):
+    question = new_question_with_incorrect_choices
+    assert not question.exist_correct_response
+    choice = Choice(
+        choice="Correct choice",
+        is_correct=True,
+        question=question
+    )
+    choice.save()
+    assert question.exist_correct_response()
