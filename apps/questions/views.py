2cafd80c0a741624b5cf599925c9149bf44afe57
Vinicius Issa <vinicius.almeidaissa@gmail.com>
2020-05-22 13:22:00 -0300

    [view][fix]Add try..except in question view
    Added Try Except in question view

--- apps/questions/views.py
+++ apps/questions/views.py
@@ -42,21 +42,24 @@ def question(request, user_id):
     answered = []
     answers = Answer.objects.filter(user_id=user_id)
 
-    for answer in answers:
-        questions = questions.exclude(id=answer.choices.question.id)
-        choice_json = ChoiceSerializer(answer.choices).data
-        answered.append(choice_json)
+    try:
+        for answer in answers:
+            questions = questions.exclude(id=answer.choices.question.id)
+            choice_json = ChoiceSerializer(answer.choices).data
+            answered.append(choice_json)
 
-    for question in questions:
-        if question.exist_correct_response():
-            choices = Choice.objects.filter(question=question)
-            question_json = QuestionSerializer(question).data
-            choices_json = ChoiceSerializer(choices, many=True).data
-            question_json['choices'] = choices_json
-            unanswered.append(question_json)
+        for question in questions:
+            if question.exist_correct_response():
+                choices = Choice.objects.filter(question=question)
+                question_json = QuestionSerializer(question).data
+                choices_json = ChoiceSerializer(choices, many=True).data
+                question_json['choices'] = choices_json
+                unanswered.append(question_json)
 
-    data = {
-        'answered': answered,
-        'unanswered': unanswered
-    }
-    return Response(data, 200)
+        data = {
+            'answered': answered,
+            'unanswered': unanswered
+        }
+        return Response(data, 200)
+    except Exception:
+        return Response({'error': 'error'}, 400)
