a44e918de464af940e1c6e784da1574b3f0bef53
Vinicius Issa <vinicius.almeidaissa@gmail.com>
2020-05-27 11:04:52 -0300

    [fix]Fix static files
    Fixed static files to run in prodction in heroku

--- quizz_backend/settings.py
+++ quizz_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -128,7 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Configure Django App for Heroku.
 import django_heroku
